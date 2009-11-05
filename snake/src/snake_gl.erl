7bdfff95aec5e04b43f8bab186003fbfbd7f0d5f
Olle Mattsson <ollemattss@gmail.com>
2009-11-06 00:25:33 +0100

    got the grid working drawing the snake and the apple properly

--- snake/src/snake_gl.erl
+++ snake/src/snake_gl.erl
@@ -15,7 +15,10 @@
 
 -record(state, {frame,
 		canvas,
-		timer}).
+		options,
+		timer,
+		snake,
+		apple}).
 
 start() ->
     start([]).
@@ -32,8 +35,7 @@ start_link(Options) ->
     wx_object:start_link(?MODULE, Options, []).
 
 init(_Options) ->
-    Frame = wxFrame:new(wx:null(), ?wxID_ANY, "Heli", [{size, {800,800}},
-						      {style, ?wxDEFAULT_FRAME_STYLE band bnot ?wxRESIZE_BORDER}]),
+    Frame = wxFrame:new(wx:null(), ?wxID_ANY, "Snake", [{size, {800,800}}]),
 
     Menu = wxMenu:new(),
 
@@ -54,10 +56,13 @@ init(_Options) ->
     wxGLCanvas:setCurrent(Canvas),
 
     init_gl(Canvas),
+    put(quadric, glu:newQuadric()),
 
     {Frame, #state{frame = Frame,
 		   canvas = Canvas,
-		   timer = Timer}}.
+		   timer = Timer,
+		   snake = {[{1,1},{2,1}], []},
+		   apple = {4,3}}}.
 
 init_gl(Canvas) ->
     {W,H} = wxWindow:getClientSize(Canvas),
@@ -79,8 +84,9 @@ init_gl(Canvas) ->
 
     wxGLCanvas:swapBuffers(Canvas).
 
-handle_event(#wx{event = #wxSize{}},State) ->
-    wxFrame:setSize(State#state.frame, {800,800}),
+
+handle_event(#wx{event = #wxSize{size = Size}},State) ->
+    resize(Size),
     draw(State),
     {noreply, State}.
 
@@ -100,43 +106,93 @@ code_change(_,_,State) ->
 terminate(_,_State) ->
     ok.
 
+resize({W, H}) ->
+    gl:viewport(0,0,W,H),
+    gl:matrixMode(?GL_PROJECTION),
+    gl:loadIdentity(),
+    glu:ortho2D(0, W, H, 0),
+    gl:matrixMode(?GL_MODELVIEW),
+    gl:loadIdentity(),
+    ok.
 
-draw(#state{canvas = Canvas}) ->
+draw(#state{canvas = Canvas, snake = Snake, apple = Apple}) ->
     gl:clear(?GL_COLOR_BUFFER_BIT),
 
     gl:matrixMode(?GL_MODELVIEW),
     gl:loadIdentity(),
 
     {W,H} = wxGLCanvas:getSize(Canvas),
-    draw_grid(W,H),
+    GridPos = draw_grid(W,H),
+    draw_snake(Snake, GridPos),
+    draw_apple(Apple, GridPos),
     wxGLCanvas:swapBuffers(Canvas).
 
+-define(SQUARE_H, 20).
+-define(SQUARE_W, 20).
+-define(LINES_V, 20).
+-define(LINES_H, 20).
 
 draw_grid(W,H) ->
+    GridW = ?SQUARE_W * ?LINES_V,
+    GridH = ?SQUARE_H * ?LINES_H,
+    GridPosX = (W-GridW) div 2,
+    GridPosY =  (H-GridH) div 2,
+    gl:pushMatrix(),
+    gl:translatef(GridPosX, GridPosY, 0),
     gl:'begin'(?GL_LINES),
     gl:color3ub(0,0,0),
-    LinesV = 20,
-    LinesH = 20,
     FunV =
 	fun(Pos) ->
-		gl:vertex2i(Pos * (W div LinesV),0),
-		gl:vertex2i(Pos * (W div LinesV),H)
+		gl:vertex2i(Pos * (GridW div ?LINES_V),0),
+		gl:vertex2i(Pos * (GridW div ?LINES_V),GridH)
 	end,
     FunH =
 	fun(Pos) ->
-		gl:vertex2i(0,Pos * (H div LinesH)),
-		gl:vertex2i(W,Pos * (H div LinesH))
+		gl:vertex2i(0,    Pos * (GridH div ?LINES_H)),
+		gl:vertex2i(GridW,Pos * (GridH div ?LINES_H))
 	end,
-    %%io:format("~p\n", [{W,H}]),
-    wx:foreach(FunV, lists:seq(0, LinesV)),
-    wx:foreach(FunH, lists:seq(0, LinesH)),
+    wx:foreach(FunV, lists:seq(0, ?LINES_V)),
+    wx:foreach(FunH, lists:seq(0, ?LINES_H)),
     gl:'end'(),
-    Quad = glu:newQuadric(),
-    gl:translatef(99,95,0),
-    glu:disk(Quad, 0, 20, 10,10),
+    gl:popMatrix(),
+    {GridPosX,GridPosY}.
 
-    glu:deleteQuadric(Quad).
+draw_snake({Head, Tail}, GridPos) ->
+    draw_snake(Head, GridPos),
+    draw_snake(Tail, GridPos);
+draw_snake([{X,Y}|Rest], GridPos = {GridX, GridY}) ->
+    gl:pushMatrix(),
+    gl:color3ub(0,0,0),
+    gl:translatef(GridX,GridY,0),
+    gl:translatef((X-1)*?SQUARE_W,(Y-1)*?SQUARE_H,0),
+    gl:'begin'(?GL_QUADS),
+ 
+    gl:vertex2i(0, 0),
+    gl:vertex2i(0, ?SQUARE_H),
+    gl:vertex2i(?SQUARE_W, ?SQUARE_H),
+    gl:vertex2i(?SQUARE_W, 0),
     
+    gl:'end'(),
+    gl:popMatrix(),
+    draw_snake(Rest, GridPos);
+draw_snake([], _) ->
+    ok.
+
+draw_apple({X,Y}, {GridX, GridY}) ->
+    gl:pushMatrix(),
+    gl:color3ub(230,50,50),
+    gl:translatef(GridX,GridY,0),
+    gl:translatef((X-1)*?SQUARE_W,(Y-1)*?SQUARE_H,0),
+    gl:'begin'(?GL_QUADS),
+
+    gl:vertex2i(0, 0),
+    gl:vertex2i(0, ?SQUARE_H),
+    gl:vertex2i(?SQUARE_W, ?SQUARE_H),
+    gl:vertex2i(?SQUARE_W, 0),
+
+    gl:'end'(),
+    gl:popMatrix().
+
 
 
 
